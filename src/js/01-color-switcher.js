caf07f9550977ca89d060acd4552cc19333434e2
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-17 00:23:12 +0100

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let startChangeColorBody = null;
 
+start.addEventListener('click', handleClickButtonStart);
+stop.addEventListener('click', handleClickButtonStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function handleClickButtonStart() {
+  startChangeColorBody = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    start.setAttribute('disabled', true);
+  }, 1000);
+}
+
+function handleClickButtonStop() {
+  clearInterval(startChangeColorBody);
+  start.removeAttribute('disabled');
+}
