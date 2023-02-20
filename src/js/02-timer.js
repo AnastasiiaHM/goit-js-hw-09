dcda832117e5a0aaa6fdb815f76d22e9b2dad84b
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-20 01:11:36 +0100

    add tasck 3 and hulf 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,94 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import '../../node_modules/notiflix/dist/notiflix-3.2.6.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const buttonStart = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+class Timer {
+  constructor({ onTick }) {
+    this.usersChosenData = null;
+    this.intervalId = null;
+    this.disabled = false;
+    this.onTick = onTick;
+  }
+
+  start() {
+    if (this.disabled) {
+      return;
+    }
+    this.intervalId = setInterval(() => {
+      const dateNow = Date.now();
+      const startTimerTime = this.usersChosenData - dateNow;
+      if (startTimerTime > 0) {
+        this.stop();
+      }
+      const timerTime = convertMs(startTimerTime);
+      onTick(timerTime);
+    }, 1000);
+  }
+  stop() {
+    clearInterval(intervalId);
+    buttonStart.removeAttribute('disabled');
+    onTick(convertMs(0));
+  }
+}
+
+const timerFromClass = new Timer({
+  onTick: updateClockFace,
+});
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    usersTime = selectedDates[0].getTime();
+    if (usersTime >= Date.now()) {
+      buttonStart.setAttribute('disabled', true);
+      Notify.warning('Please choose a date in the future');
+      return;
+    }
+    timerFromClass.usersChosenData = usersTime;
+  },
+};
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  daysSpan.textContent = `${days}`;
+  hoursSpan.textContent = `${hours}`;
+  minutesSpan.textContent = `${minutes}`;
+  secondsSpan.textContent = `${seconds}`;
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const timerId = flatpickr('#datetime-picker', options);
+
+buttonStart.addEventListener('click', () => {
+  timerFromClass.start();
+  buttonStart.setAttribute('disabled', true);
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import '../../node_modules/notiflix/dist/notiflix-3.2.6.min.css';
+
+const formElement = document.querySelector('.form');
+const delayElement = document.querySelector('[name="delay"]');
+const stepElement = document.querySelector('[name="step"]');
+const amountElement = document.querySelector('[name="amount"]');
+
+function handleSubmitFofm(event) {
+  event.preventDefault();
+  const delay = Number(delayElement.value);
+  const step = Number(stepElement.value);
+  const amount = Number(amountElement.value);
+
+  let position = 0;
+  for (let i = 0; i <= amount; i += 1) {
+    let usersTime = delay + i * step;
+    position += 1;
+    const promise = createPromise(position, usersTime);
+
+    promise
+      .then(success => {
+        Notify.success(success);
+      })
+      .catch(error => {
+        Notify.failure(error);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve`✅ Fulfilled promise ${position} in ${delay}ms`;
+      }
+      reject`❌ Rejected promise ${position} in ${delay}ms`;
+    }, delay);
+  });
+}
+
+formElement.addEventListener('submit', handleSubmitFofm);
