3ea978b7991cc5a853e7053e59cfff776f2dbe29
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-21 12:18:59 +0000

    Deploying to gh-pages from @ AnastasiiaHM/goit-js-hw-09@62ddc43c69b11412bfcdd7d06c26817d5dd26088 🚀

--- 01-color-switcher.7126bd21.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(function(){r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.7126bd21.js.map
