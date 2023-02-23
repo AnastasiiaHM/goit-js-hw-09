abcff48b44bb7af46d02f76a4240f65e2714f959
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-02-23 14:36:00 +0100

    fixed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,8 +10,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 function handleClickButtonStart() {
+  start.setAttribute('disabled', true);
   startChangeColorBody = setInterval(() => {
-    start.setAttribute('disabled', true);
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
